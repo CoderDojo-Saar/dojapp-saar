5645853eab53dece05a469b26a3f419575994dc3
cte4X <moritzruthcode@gmail.com>
2017-11-25 19:38:23 +0100

    JS neu formatiert

--- js/begruessung.js
+++ js/begruessung.js
@@ -3,9 +3,9 @@
 if ('speechSynthesis' in window) {
     var synth = window.speechSynthesis;
     var speakThis = new SpeechSynthesisUtterance('Willkommen beim CoderDojo Saar');
-    
+
     // Zufällige Stimme :)
     //speakThis.voice = synth.getVoices()[Math.floor(Math.random(1) * (synth.getVoices().length - 1))];
-    
+
     synth.speak(speakThis);
 }
--- js/sidemenu.js
+++ js/sidemenu.js
@@ -2,15 +2,15 @@
 
 function createSideMenu(activeMenuId) {
     var menu = [
-        { id: 'start', label: 'Start', link: 'index.html' },
-        { id: 'about', label: 'Über', link: 'about.html' },
-        { id: 'registration', label: 'Anmeldung', link: 'anmeldung.html' },
-        { id: 'geo', label: 'Anfahrt', link: 'geonavigation.html' },
-        { id: 'contact', label: 'Kontaktm&ouml;glichkeiten', link: 'kontaktmöglichkeiten.html' },
-        { id: 'gallery', label: 'Bildergalerie', link: 'bildergalerie.html' },
+        {id: 'start', label: 'Start', link: 'index.html'},
+        {id: 'about', label: 'Über', link: 'about.html'},
+        {id: 'registration', label: 'Anmeldung', link: 'anmeldung.html'},
+        {id: 'geo', label: 'Anfahrt', link: 'geonavigation.html'},
+        {id: 'contact', label: 'Kontaktm&ouml;glichkeiten', link: 'kontaktmöglichkeiten.html'},
+        {id: 'gallery', label: 'Bildergalerie', link: 'bildergalerie.html'},
         {},
-        { id: 'sponsors', label: 'Sponsoren', link: 'sponsoring.html' },
-        { id: 'legal', label: 'Impressum', link: 'impressum.html' },
+        {id: 'sponsors', label: 'Sponsoren', link: 'sponsoring.html'},
+        {id: 'legal', label: 'Impressum', link: 'impressum.html'},
     ];
 
     // @ts-ignore
