1c16b1acc049a0febb647430761c5107f256ba0b
cte4X <moritzruthcode@gmail.com>
2018-01-28 14:52:07 +0100

    kleiner fix

--- www/js/seite.js
+++ www/js/seite.js
@@ -3,7 +3,7 @@ $(document).ready(onDocumentLoad);
 function onDocumentLoad() {
     // Erstellt das Menü
     createSideMenu(activeMenuId);
-    if (typeof createSponsorList !== undefined) {
+    if (typeof createSponsorList !== 'undefined') {
         createSponsorList();
     }
 }
