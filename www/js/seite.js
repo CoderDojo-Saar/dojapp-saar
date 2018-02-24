dd72f70c23453ca2cd9b5c4845124a70756dfbb2
Moritz Ruth <moritzruthcode@gmail.com>
2018-02-24 15:44:21 +0100

    Changelog nach Update; cordova-android Update
    closes #11

--- www/js/seite.js
+++ www/js/seite.js
@@ -1,8 +1,21 @@
+'use strict';
+
+const THIS_VERSION = "0.1";
 
 $(document).ready(function () {
+    let storage = window.localStorage;
+
     // Erstellt das Menü
     createSideMenu(activeMenuId);
     if (typeof createSponsorList !== 'undefined') {
         createSponsorList();
+    }
+
+    let storageVersion = storage.getItem("version");
+
+    if(storageVersion !== THIS_VERSION) {
+        $.getScript("js/showChangelog.js");
+
+        storage.setItem("version", THIS_VERSION.toString());
     }
 });
---
+++ www/js/showChangelog.js
@@ -0,0 +1,12 @@
+UIkit.modal.dialog('<div class="uk-modal-body">' +
+        '<h2 class="uk-modal-title">Changelog</h2>' +
+        '<ul class="uk-list uk-list-bullet">' +
+            '<li>Sponsoren' +
+            '<li>Immer aktueller Countdown</li>' +
+            '<li>Diesen Changelog</li>' +
+            '</li>' +
+        '</ul>' +
+        '<p class="uk-text-right">' +
+            '<button class="uk-button uk-button-primary uk-modal-close" type="button">Okay, schließen</button>' +
+        '</p>' +
+    '</div>');
